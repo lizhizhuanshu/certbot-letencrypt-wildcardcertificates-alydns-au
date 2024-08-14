b7ef580d2f7c2b367abbb9002291b06870ae39e3
Ponder Lee <lizhizhuanshu@gmail.com>
2024-08-14 16:57:06 +0800

    add other provider

--- au.sh
+++ au.sh
@@ -87,7 +87,7 @@ case $plang in
 		dnsapi=$PATH"/python-version/alydns.py"
 		key=$ALY_KEY
 		token=$ALY_TOKEN
-    elif [[ "$pdns" == "txy" ]] ;then
+  elif [[ "$pdns" == "txy" ]] ;then
 		dnsapi=$PATH"/python-version/txydns.py"
 		key=$TXY_KEY
 		token=$TXY_TOKEN
--- auto-ssl.sh
+++ auto-ssl.sh
@@ -2,6 +2,7 @@
 
 
 AU_FILE=$(pwd)/au.sh
+CONFIG_FILE=$(pwd)/config.sh
 
 # 查找并替换 TXY_KEY,TXY_TOKEN
 
@@ -37,11 +38,54 @@ init(){
   sudo apt install python-is-python3
 }
 
+
+ensureContext(){
+  read -p 'Please select your domain service provider,default is 1: 
+  1.腾讯云 
+  2.阿里云 
+  3.华为云 
+  4.GoDaddy
+  ' PROVIDER
+
+  if [ ! $PROVIDER ]; then
+    PROVIDER=1
+  fi
+
+  if [ ! -f $CONFIG_FILE ]
+  then
+    echo "" > $CONFIG_FILE
+  fi
+
+  case $PROVIDER in
+    1)PROVIDER_NAME="txy"
+      ensure_field "TXY_KEY" $CONFIG_FILE
+      ensure_field "TXY_TOKEN" $CONFIG_FILE
+      ;;
+    2)PROVIDER_NAME="aly" 
+      ensure_field "ALY_KEY" $CONFIG_FILE
+      ensure_field "ALY_TOKEN" $CONFIG_FILE
+      ;;
+    3)PROVIDER_NAME="hwy"
+      ensure_field "HWY_KEY" $CONFIG_FILE
+      ensure_field "HWY_TOKEN " $CONFIG_FILE
+      ;;
+    4)PROVIDER_NAME="godaddy"
+      ensure_field "GODADDY_KEY" $CONFIG_FILE
+      ensure_field "GODADDY_TOKEN " $CONFIG_FILE
+      ;;
+    *)
+      echo "Not support this dns services"
+      exit
+  esac
+}
+
 install(){
   read -p "Please input your domain name:" DOMAIN_NAME
+
+  ensureContext
   CURRENT_DIR=$(pwd)
   #申请域名证书
-  sudo certbot certonly  -d ${DOMAIN_NAME} -d *.${DOMAIN_NAME} --manual --preferred-challenges dns  --manual-auth-hook "${AU_FILE} python txy add" --manual-cleanup-hook "${AU_FILE} python txy clean"
+  sudo certbot certonly  -d ${DOMAIN_NAME} -d *.${DOMAIN_NAME} --manual --preferred-challenges dns  --manual-auth-hook "${AU_FILE} python ${PROVIDER_NAME} add" --manual-cleanup-hook "${AU_FILE} python ${PROVIDER_NAME} clean"
   echo "证书申请成功 请查看 /etc/letsencrypt/live/${DOMAIN_NAME} 目录"
   echo "使用证书 cer /etc/letsencrypt/live/${DOMAIN_NAME}/fullchain.pem"
   echo "使用证书 key /etc/letsencrypt/live/${DOMAIN_NAME}/privkey.pem"
@@ -52,10 +96,11 @@ task_cmd(){
     HEADER=sudo
   fi
   read -p "Please input your domain name:" DOMAIN_NAME
+  ensureContext
   if [ -f "action.sh" ]; then
     DEPLAY_HOOK="--deploy-hook  \"/bin/bash $USER $(pwd)/action.sh\""
   fi
-  TASK_CMD="${HEADER} certbot renew --cert-name ${DOMAIN_NAME} $DEPLAY_HOOK --manual-auth-hook \"${AU_FILE} python txy add\" --manual-cleanup-hook \"${AU_FILE} python txy clean\""
+  TASK_CMD="${HEADER} certbot renew --cert-name ${DOMAIN_NAME} $DEPLAY_HOOK --manual-auth-hook \"${AU_FILE} python ${PROVIDER_NAME} add\" --manual-cleanup-hook \"${AU_FILE} python ${PROVIDER_NAME} clean\""
 }
 
 add_task(){
